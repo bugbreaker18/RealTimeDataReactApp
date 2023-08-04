13ffcea47fc1a8e6d8e409454f031b5d0299fb8d
Shreyansh Shukla <bugbreaker18@gmail.com>
2023-08-04 17:58:59 +0000

    Now we see a chart with interactions

--- real-time-currencies/src/app/page.js
+++ real-time-currencies/src/app/page.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import styles from './page.module.css'
 import { useState, useEffect } from 'react'
 import { resolve } from 'styled-jsx/css';
+import LineChart from './components/LineChart';
 
 export default function Home() {
   const [stockinfo, setstockinfo] = useState([])
@@ -33,7 +34,7 @@ export default function Home() {
   //
   return (
     <main className={styles.main}>
-      <h1>Hello World!</h1>
+      <LineChart />
     
 
     </main>
